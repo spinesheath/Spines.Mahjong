e57394e3548c5bf8d2a6646af59ffecb53157daa
Johannes Heckl <heckl.johannes@googlemail.com>
2021-03-15 16:23:00 +0100

    A few uke ire tests

---
+++ Analysis.Tests/UkeIreTests.cs
@@ -0,0 +1,21 @@
+﻿using Spines.Mahjong.Analysis.Shanten;
+using Xunit;
+
+namespace Spines.Mahjong.Analysis.Tests
+{
+  public class UkeIreTests
+  {
+    [Theory]
+    [InlineData("1112345678999m", new[] {1, 3, 3, 3, 3, 3, 3, 3, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1})]
+    [InlineData("1m111M222M333M444M", new[] { -1, -1, -1, -1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 })]
+    public void JustSomeHands(string hand, int[] expected)
+    {
+      var parser = new ShorthandParser(hand);
+      var c = new HandCalculator(parser);
+
+      var actual = c.GetUkeIreFor13();
+
+      Assert.Equal(expected, actual);
+    }
+  }
+}
