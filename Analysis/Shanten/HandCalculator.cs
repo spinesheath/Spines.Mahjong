2c539e3c3809125294d35584bff24298a003cd67
Johannes Heckl <heckl.johannes@googlemail.com>
2021-04-24 00:30:46 +0200

    Fix bug in furiten calculation

--- Analysis/Shanten/HandCalculator.cs
+++ Analysis/Shanten/HandCalculator.cs
@@ -239,9 +239,9 @@ namespace Spines.Mahjong.Analysis.Shanten
       Debug.Assert(TilesInHand() == 13 && Shanten == 0, "furiten only makes sense at tenpai");
 
       var ukeIre = GetUkeIreFor13();
-      foreach (var i in ukeIre)
+      for (var i = 0; i < 34; i++)
       {
-        if (i >= 0)
+        if (ukeIre[i] >= 0)
         {
           yield return TileType.FromTileTypeId(i);
         }
