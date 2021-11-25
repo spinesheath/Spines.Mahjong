517b0aff2d51b603ded04e8f9d4667c6c8842bd7
Johannes Heckl <heckl.johannes@googlemail.com>
2021-11-26 00:45:59 +0100

    shanten5 for chiitoi and kokushi

--- Analysis/Shanten5/Shanten5Calculator.cs
+++ Analysis/Shanten5/Shanten5Calculator.cs
@@ -31,25 +31,29 @@ namespace Spines.Mahjong.Analysis.Shanten5
       var m = LookupSuit[base5Hashes[0]];
       var p = LookupSuit[base5Hashes[1]];
       var s = LookupSuit[base5Hashes[2]];
-      var z = LookupSuit[base5Hashes[3]];
+      var z = LookupHonor[base5Hashes[3]];
       var a = CalculatePhase1(m, p);
       var b = CalculatePhase1(s, z);
 
-      var sb = Vector128.Create(9, 8, 7, 6, 5, 4, 3, 2, 1, 255, 255, 255, 255, 255, 255, 255);
+      var sb = Vector128.Create(9, 8, 7, 6, 5, 4, 3, 2, 1, 255, 10, 255, 255, 13, 255, 14);
       var b2 = Ssse3.Shuffle(b, sb);
 
       // TODO meldCount
 
       var r = Sse2.Add(a, b2);
 
-      // horizontal max
-      var neg = Vector128.Create((byte)14);
+      var neg = Vector128.Create(14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 7, 255, 255, 14, 254, 254);
       var r1 = Sse2.Subtract(neg, r);
       var r3 = Sse2.ShiftRightLogical(r1.AsInt16(), 8);
       var r4 = Sse2.Min(r1, r3.AsByte());
-      var r5 = Sse41.MinHorizontal(r4.AsUInt16());
-      var r6 = (byte)Sse2.ConvertToInt32(r5.AsInt32());
-      return r6 - 1;
+      var k1 = Vector128.Create((byte) 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0);
+      var k2 = Sse2.And(r4, k1);
+      var k3 = Sse2.ShiftRightLogical(k2.AsUInt32(), 16);
+      var k4 = k3.AsByte();
+      var r5 = Sse2.Subtract(r4, k4);
+      var r6 = Sse41.MinHorizontal(r5.AsUInt16());
+      var r7 = (byte)Sse2.ConvertToInt32(r6.AsInt32());
+      return r7 - 1;
     }
 
     /// <summary>
@@ -59,9 +63,9 @@ namespace Spines.Mahjong.Analysis.Shanten5
     {
       // first calculate all the sums, then merge them down with repeated vertical max
 
-      var sa1 = Vector128.Create(1, 2, 3, 4, 1, 2, 3, 1, 1, 2, 1, 1, 2, 1, 255, 255);
+      var sa1 = Vector128.Create((byte)1, 2, 3, 4, 1, 2, 3, 1, 1, 2, 1, 1, 2, 1, 13, 15);
       var va1 = Ssse3.Shuffle(a, sa1);
-      var sb1 = Vector128.Create(8, 7, 6, 5, 7, 6, 5, 1, 6, 5, 5, 3, 2, 2, 255, 255);
+      var sb1 = Vector128.Create((byte)8, 7, 6, 5, 7, 6, 5, 1, 6, 5, 5, 3, 2, 2, 13, 15);
       var vb1 = Ssse3.Shuffle(b, sb1);
 
       var vab1 = Sse2.Add(va1, vb1);
@@ -75,7 +79,7 @@ namespace Spines.Mahjong.Analysis.Shanten5
 
       var xab1 = Sse2.Max(vab1, vab2);
 
-      var sab11 = Vector128.Create(0, 1, 8, 10, 255, 11, 13, 7, 255, 255, 4, 6, 255, 255, 255, 255);
+      var sab11 = Vector128.Create(0, 1, 8, 10, 255, 11, 13, 7, 255, 255, 4, 6, 255, 255, 14, 15);
       var sab12 = Vector128.Create(2, 3, 9, 255, 255, 12, 255, 255, 255, 255, 5, 255, 255, 255, 255, 255);
 
       var vab21 = Ssse3.Shuffle(xab1, sab11);
@@ -83,7 +87,7 @@ namespace Spines.Mahjong.Analysis.Shanten5
 
       var xab2 = Sse2.Max(vab21, vab22);
 
-      var sab21 = Vector128.Create(255, 255, 7, 6, 5, 255, 3, 2, 10, 0, 255, 255, 255, 255, 255, 255);
+      var sab21 = Vector128.Create(255, 255, 7, 6, 5, 255, 3, 2, 10, 0, 15, 255, 255, 14, 255, 255);
       var sab22 = Vector128.Create(255, 255, 255, 255, 255, 255, 255, 255, 11, 1, 255, 255, 255, 255, 255, 255);
 
       var vab31 = Ssse3.Shuffle(xab2, sab21);
